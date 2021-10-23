cf12c9d15ca5b9f01851ad17715bc687599da5b3
Franco1230 <franco.hfh1230@gmail.com>
2021-10-24 00:29:47 +1100

    fixed mission_to_mars.ipynb

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,39 +1,35 @@
-# MongoDB and Flask Application
-#################################################
-
-# Dependencies and Setup
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
 
-#################################################
-# Flask Setup
-#################################################
+# Create an instance of Flask
 app = Flask(__name__)
 
-#################################################
-# PyMongo Connection Setup
-#################################################
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mission_to_mars"
-mongo = PyMongo(app)
+# Use PyMongo to establish Mongo connection
+mongo = PyMongo(app, uri = "mongodb://localhost:27017/mission_to_mars_db")
 
-#################################################
-# Flask Routes
-#################################################
-# Root Route to Query MongoDB & Pass Mars Data Into HTML Template: index.html to Display Data
+# Create route that renders index.html template and finds data from mongo
 @app.route("/")
-def index():
-    mars = mongo.db.mars.find_one()
-    return render_template("./index.html", mars=mars)
+def home(): 
 
-# Scrape Route to Import `nasa_scraper.py` Script & Call `scrape` Function
+    # Find data
+    mars_facts = mongo.db.collection.find_one()
+
+    # Return template and data
+    return render_template("index.html", mars = mars_facts)
+
+# Route that will trigger the scrape function
 @app.route("/scrape")
-def scrapper():
-    mars = mongo.db.mars
-    mars_data = scrape_mars.scrape_all()
-    mars.update({}, mars_data, upsert=True)
-    return "Scraping Successful"
+def scrape():
 
-# Define Main Behavior
+    # Run the scrape function
+    mars_data = scrape_mars.scrape()
+
+    # Update the Mongo database using update and upsert=True
+    mongo.db.collection.update({}, mars_data, upsert = True)
+
+    # Redirect back to home page
+    return redirect("/")
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -8,126 +8,133 @@ from pprint import pprint
 # Define function to choose the executable path
 def init_browser():
     executable_path = {"executable_path": ChromeDriverManager().install()}
-    return Browser("chrome", **executable_path, headless = False)
+    return Browser("chrome", **executable_path, headless = False, user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36")
 
-""" NASA Mars News """
-# Connect to Mars News Site
-browser = init_browser()
+# Full Scrape function.
+def scrape():
 
-mars_news_url = "http://redplanetscience.com"
-browser.visit(mars_news_url)
-
-# HTML Object
-html = browser.html
-
-# Parse HTML with Beautiful Soup
-news_soup = bs(html, "html.parser")    
-
-# Retrieve the latest article's title
-news_title = news_soup.find("div", class_ = "content_title")
-news_title = news_title.text.strip()
-print(news_title)
-
-# Retrieve the latest article's paragraph
-news_paragraph = news_soup.find("div", class_ = "article_teaser_body")
-news_paragraph = news_paragraph.text.strip()
-print(news_paragraph)
-
-# Exit Browser
-browser.quit()
-
-""" JPL Mars Space Images - Featured Image """
-# Connect to the image URL
-browser = init_browser()
-
-mars_featured_image_url = "http://spaceimages-mars.com/"
-browser.visit(mars_featured_image_url)
-
-# HTML Object
-html = browser.html
-
-# Parse HTML with Beautiful Soup
-image_soup = bs(html, "html.parser")
-
-# Assign the full url string to a variable called "featured_image_url"
-featured_image = image_soup.find("img", class_ = "headerimage fade-in")
-featured_image_url = mars_featured_image_url + featured_image["src"]
-print(featured_image_url)
-
-# Exit Browser
-browser.quit()
-
-""" Mars Facts """
-# URL for Mars Facts.
-mars_facts_url = "http://space-facts.com/mars/"
-
-# Use Pandas to convert the data to a HTML table string
-mars_facts = pd.read_html(mars_facts_url)
-
-# Save as DataFrame
-mars_facts_df = pd.DataFrame(mars_facts[1])
-
-#  Save DataFrame to html
-mars_facts = mars_facts_df.to_html(header = True, index = True)
-
-""" Mars Hemispheres """
-# Connect to the image URL
-browser = init_browser()
-
-mars_hemispheres_url = "http://marshemispheres.com/"
-browser.visit(mars_hemispheres_url)
-
-# Parse HTML with Beautiful Soup
-hemispheres_soup = bs(html, "html.parser")
-
-# HTML Object
-html = browser.html
-
-# Each link is located in "div" tag, class "item"
-# Locate all 4 elements and store in variable.
-hems_url = hemispheres_soup.find_all("div", class_ = "item")
-
-# Create empty list for each Hemisphere URL
-hemis_url = []
-
-for hem in hems_url:
-    hem_url = hem.find("a")["href"]
-    hemis_url.append(hem_url)
-
-# Exit Browser
-browser.quit()
-
-# Create list of dictionaries called hemisphere_image_urls
-hemisphere_image_urls = []
-
-# Iterate through all URLs saved in hemis_url
-for hemi in hemis_url:
-    mars_hem_url = mars_hemispheres_url + hemi
-    print(mars_hem_url)
-    
-    # Run init_browser/driver.
+    """ NASA Mars News """
+    # Connect to Mars News Site
     browser = init_browser()
-    browser.visit(mars_hem_url)
-    
+
+    mars_news_url = "http://redplanetscience.com"
+    browser.visit(mars_news_url)
+
+    # HTML Object
+    html = browser.html
+
+    # Parse HTML with Beautiful Soup
+    news_soup = bs(html, "html.parser")    
+
+    # Retrieve the latest article's title
+    news_title = news_soup.find("div", class_ = "content_title")
+    news_title = news_title.text.strip()
+    print(news_title)
+
+    # Retrieve the latest article's paragraph
+    news_paragraph = news_soup.find("div", class_ = "article_teaser_body")
+    news_paragraph = news_paragraph.text.strip()
+    print(news_paragraph)
+
+    # Exit Browser
+    browser.quit()
+
+    """ JPL Mars Space Images - Featured Image """
+    # Connect to the image URL
+    browser = init_browser()
+
+    mars_featured_image_url = "http://spaceimages-mars.com/"
+    browser.visit(mars_featured_image_url)
+
+    # HTML Object
+    html = browser.html
+
+    # Parse HTML with Beautiful Soup
+    image_soup = bs(html, "html.parser")
+
+    # Assign the full url string to a variable called "featured_image_url"
+    featured_image = image_soup.find("img", class_ = "headerimage fade-in")
+    featured_image_url = mars_featured_image_url + featured_image["src"]
+    print(featured_image_url)
+
+    # Exit Browser
+    browser.quit()
+
+    """ Mars Facts """
+    # URL for Mars Facts.
+    mars_facts_url = "http://space-facts.com/mars/"
+
+    # Use Pandas to convert the data to a HTML table string
+    mars_facts = pd.read_html(mars_facts_url)
+
+    # Save as DataFrame
+    mars_facts_df = pd.DataFrame(mars_facts[1])
+
+    #  Save DataFrame to html
+    mars_facts = mars_facts_df.to_html(header = True, index = True)
+
+    """ Mars Hemispheres """
+    # Run init_browser/driver
+    browser = init_browser()
+
+    # Visit the url for Mars Hemisphere
+    mars_hemispheres_url = "http://marshemispheres.com/"
+    browser.visit(mars_hemispheres_url)
+
     # HTML Object.
     html = browser.html
 
     # Parse HTML with Beautiful Soup
-    hemi_soup = bs(html, "html.parser")
+    hemispheres_soup = bs(html, "html.parser")
 
-    # Locate each title and save to raw_title, to be cleaned.
-    raw_title = hemi_soup.find("h2", class_="title").text
-    
-    # Remove " Enhanced" tag text from each "title" via split on " Enhanced".
-    title = raw_title.split(" Enhanced")[0]
-    
-    # Locate each full resolution image for all 4 Hemisphere URLs.
-    img_url = hemi_soup.find("img", class_ = "wide-image")["src"]
-    
-    # Append both title and img_url to "hemisphere_image_url".
-    hemisphere_image_urls.append({"title": title, "img_url": mars_hemispheres_url + img_url})
-    
+    # Each link is located in "div" tag, class "item"
+    # Locate all 4 elements and store in variable
+    hems_url = hemispheres_soup.find_all("div", class_ = "item")
+
     # Exit Browser
     browser.quit()
 
-    pprint(hemisphere_image_urls)
+    # Create empty list for each Hemisphere URL
+    hemis_url = []
+
+    for hem in hems_url:
+        hem_url = hem.find("a")["href"]
+        hemis_url.append(hem_url)
+
+    # Exit Browser
+    browser.quit()
+
+    # Create list of dictionaries called hemisphere_image_urls
+    hemisphere_image_urls = []
+
+    # Iterate through all URLs saved in hemis_url
+    for hemi in hemis_url:
+        mars_hem_url = mars_hemispheres_url + hemi
+        print(mars_hem_url)
+        
+        # Run init_browser/driver.
+        browser = init_browser()
+        browser.visit(mars_hem_url)
+        
+        # HTML Object.
+        html = browser.html
+
+        # Parse HTML with Beautiful Soup
+        hemi_soup = bs(html, "html.parser")
+
+        # Locate each title and save to raw_title, to be cleaned.
+        raw_title = hemi_soup.find("h2", class_="title").text
+        
+        # Remove " Enhanced" tag text from each "title" via split on " Enhanced".
+        title = raw_title.split(" Enhanced")[0]
+        
+        # Locate each full resolution image for all 4 Hemisphere URLs.
+        img_url = hemi_soup.find("img", class_ = "wide-image")["src"]
+        
+        # Append both title and img_url to "hemisphere_image_url".
+        hemisphere_image_urls.append({"title": title, "img_url": mars_hemispheres_url + img_url})
+        
+        # Exit Browser
+        browser.quit()
+
+        print(hemisphere_image_urls)
